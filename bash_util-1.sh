d8666d37983737cbefee1b322396f4916cd8c87f
Navneet <navneet.sahay@gmail.com>
2022-02-11 11:23:39 -0500

    Create bash_util-1.sh

---
+++ bash_util-1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+OLDIFS=$IFS; IFS="," #Input field separator, we use to parse csv
+echo -e "\e[96m =============== \e[0m\n" #green
+echo -e "\e[1;96m =============== \e[0m\n" #green bold
